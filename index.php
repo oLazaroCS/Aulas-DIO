361319f6edebc474323032e450dbbb1e9be9e240
oLazaroCS <lazaro.5030@hotmail.com>
2022-05-13 12:55:00 -0300

    Arquivos da aula de HTML

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Meu site PHP</title>
+	</head>
+	
+	<body>
+		<?php
+			for ( $i = 0 ; $i <10 ; $i++ ) {
+				print( "Linhas numero " .$i . "<br />");
+			}
+			?>
+	</body>
+</html>
